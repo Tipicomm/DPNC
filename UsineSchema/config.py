2a957aafc35b4980a5e6954c365fc04e45cfac92
Tipicomm <101728993+Tipicomm@users.noreply.github.com>
2025-10-06 14:47:30 +0200

    Create config.py

---
+++ UsineSchema/config.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# UsineSchema/config.py
+
+# Identifiants
+DATASET_ID = "6842b8e772325215e9dbf196"
+RESOURCE_ID = "ad59533c-1c18-4eb4-a079-7e061ec5dbcd"
+
+# Fichiers
+RESOURCE_contexte = f"UsineSchema/schema_{DATASET_ID}_{RESOURCE_ID}.csv"
+OUTPUT = f"UsineSchema/schema_enrichi_{DATASET_ID}_{RESOURCE_ID}.csv"
